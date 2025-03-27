c2b379cdc2591628a14a91e2bc78bd52c4cee8c2
Nandu <nandakumar9895776488@gmail.com>
2025-03-27 14:36:20 +0530

    stackUQueue

--- p.js
+++ p.js
@@ -77,6 +77,96 @@
 
 // mergeSort
 
-function mergeSort(arr){
-    let 
+// function mergeSort(arr){
+//     if(arr.length<2){
+//         return arr
+//     }
+//     let mid=Math.floor(arr.length/2)
+//     let left=arr.slice(0,mid)
+//     let right=arr.slice(mid)
+//     return merge(mergeSort(left),mergeSort(right))
+// }
+// function merge(left,right){
+//     let sorted=[]
+//     while(left.length && right.length){
+//         if(left[0]<=right[0]){
+//             sorted.push(left.shift())
+//         }else{
+//             sorted.push(right.shift())
+//         }
+//     }
+//     return [...sorted,...left,...right]
+// }
+// console.log(mergeSort([3,5,8,2,4,72]))
+
+// ---------------------------------------------------------
+
+// stack using  Queue
+
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+    enqueue(element) {
+        this.items.push(element);
+    }
+    dequeue() {
+        return this.items.shift();
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+    peek() {
+        if (!this.isEmpty()) {
+        return  this.items[0];
+        }
+        return null;
+    }
+    getSize() {
+        return  this.items.length;
+    }
+    print() {
+        console.log(this.items.toString());
+    }
 }
+
+class Stack{
+    constructor(){
+        this.queue1=new Queue()
+        this.queue2=new Queue()
+    }
+    push(value){
+
+          // Move all elements from queue1 to queue2
+        while(!this.queue1.isEmpty()){
+            this.queue2.enqueue(this.queue1.dequeue())
+        }
+        // Push the new element into queue1
+        this.queue1.enqueue(value)
+
+         // Move all elements back from queue2 to queue1
+        while(!this.queue2.isEmpty()){
+            this.queue1.enqueue(this.queue2.dequeue())
+        }
+    }
+    pop(){
+        if(this.queue1.isEmpty()){
+            return "underflow"
+        }
+        return this.queue1.dequeue()
+    }
+    front(){
+        return this.queue1.peek()
+    }
+    display(){
+        console.log("elements are ", this.queue1.items.toString());
+        
+    }
+}
+
+
+let stack =new Stack()
+stack.push(5)
+stack.push(66)
+stack.push(2)
+console.log(stack.display())
