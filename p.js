1c11a27a652cb0c5ed5bed46812c357e67614afe
Nandu <nandakumars5352@gmail.com>
2025-04-29 21:36:37 +0530

    //

--- p.js
+++ p.js
@@ -397,3 +397,25 @@ function  remove(arr){
 // console.log((two()));
 
 ///////////////////////////////////
+// function hello(){
+//     let a=10
+//     let b=20
+//     let c=30
+// // return largest(a,b,c)
+//   console.log(largest(a,b,c))
+// }
+
+// function largest(a,b,c){
+//     let large=a
+//     if(b>large){
+//         large=b
+//     }
+//     if(c>large){
+//         large=c
+//     }
+//      return large
+// }
+// console.log(hello());
+
+
+////////////////////
